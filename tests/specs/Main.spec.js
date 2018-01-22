b3640b81ca70ab24dafb2fe668c7bb1cefc2c31a
Darlan Caruso <devcaruso@gmail.com>
2018-01-22 21:18:04 -0200

    Add title storybook

--- tests/specs/Main.spec.js
+++ tests/specs/Main.spec.js
@@ -8,4 +8,19 @@ describe('FullHeader', () => {
     const wrapper = shallow(<FullHeader />);
     expect(wrapper.find('header')).to.have.length(1);
   });
+
+  it('should have the tag h1 when title passed', () => {
+    const wrapper = shallow(<FullHeader title="TDD" />);
+    expect(wrapper.find('h1')).to.have.length(1);
+  });
+
+  it('should not have the tag h1 when title is not passed', () => {
+    const wrapper = shallow(<FullHeader />);
+    expect(wrapper.find('h1')).to.have.length(0);
+  });
+
+  it('should have the correct content is title is passed', () => {
+    const wrapper = shallow(<FullHeader title="TDD" />);
+    expect(wrapper.find('h1').props().children).to.be.equal('TDD');
+  });
 });
